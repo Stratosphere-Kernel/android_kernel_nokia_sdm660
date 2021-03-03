1526c0a06d2c0bda019c40f48e492903e05c4487
taalojarvi <sreedevan05@gmail.com>
2021-03-03 17:26:20 +0530

    Revert "cpu-boost: Pull in Modified version for EAS"
    This reverts commit babbc85cf533729aa3c74357a770f044a2ab8575.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -38,14 +38,11 @@ static bool input_boost_enabled;
 static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
 
-<<<<<<< HEAD
 static bool sched_boost_on_input;
 module_param(sched_boost_on_input, bool, 0644);
 
 static bool sched_boost_active;
 
-=======
->>>>>>> 2c8283ff4c2... cpu-boost: Pull in Modified version for EAS
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
 #define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
@@ -174,11 +171,7 @@ static void update_policy_online(void)
 
 static void do_input_boost_rem(struct work_struct *work)
 {
-<<<<<<< HEAD
 	unsigned int i, ret;
-=======
-	unsigned int i;
->>>>>>> 2c8283ff4c2... cpu-boost: Pull in Modified version for EAS
 	struct cpu_sync *i_sync_info;
 
 	/* Reset the input_boost_min for all CPUs in the system */
@@ -191,20 +184,16 @@ static void do_input_boost_rem(struct work_struct *work)
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
-<<<<<<< HEAD
 	if (sched_boost_active) {
 		ret = sched_set_boost(0);
 		if (ret)
 			pr_err("cpu-boost: HMP boost disable failed\n");
 		sched_boost_active = false;
 	}
-=======
->>>>>>> 2c8283ff4c2... cpu-boost: Pull in Modified version for EAS
 }
 
 static void do_input_boost(struct work_struct *work)
 {
-<<<<<<< HEAD
 	unsigned int i, ret;
 	struct cpu_sync *i_sync_info;
 
@@ -213,12 +202,6 @@ static void do_input_boost(struct work_struct *work)
 		sched_set_boost(0);
 		sched_boost_active = false;
 	}
-=======
-	unsigned int i;
-	struct cpu_sync *i_sync_info;
-
-	cancel_delayed_work_sync(&input_boost_rem);
->>>>>>> 2c8283ff4c2... cpu-boost: Pull in Modified version for EAS
 
 	/* Set the input_boost_min for all CPUs in the system */
 	pr_debug("Setting input boost min for all CPUs\n");
@@ -230,7 +213,6 @@ static void do_input_boost(struct work_struct *work)
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
-<<<<<<< HEAD
 	/* Enable scheduler boost to migrate tasks to big cluster */
 	if (sched_boost_on_input) {
 		ret = sched_set_boost(1);
@@ -240,8 +222,6 @@ static void do_input_boost(struct work_struct *work)
 			sched_boost_active = true;
 	}
 
-=======
->>>>>>> 2c8283ff4c2... cpu-boost: Pull in Modified version for EAS
 	queue_delayed_work(cpu_boost_wq, &input_boost_rem,
 					msecs_to_jiffies(input_boost_ms));
 }
